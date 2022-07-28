84a43aeef639769e1bfd50be267bbc279bc3a3eb
TSdnl13 <daniel.int@outlook.com>
2022-07-28 11:50:18 -0500

    Add UserServise.js file
    In this file, there are functions that import mysql2 pool connection and
    use it to make database queries form users table. the functions that make that queries are:
     -findUserByEmail, this function brings a user that have some email.
     -saveUser, this function save some user.

---
+++ backend/src/services/UserService.js
@@ -0,0 +1,14 @@
+import { pool } from '../config/DatabseConfig.js';
+
+export const findUserByEmail = async (email) => {
+   return await pool.query('SELECT * FROM users WHERE email=?', [email]);
+};
+
+export const saveUser = async (user) => {
+   return await pool
+      .query(
+         'INSERT INTO users (first_name, last_name, cellphone, address, email, password) ' +
+         ' VALUES (?, ?, ?, ?, ?, ?)', 
+         [user.firstName, user.lastName, user.cellphone, user.address, user.email, user.password]
+      );
+};
