56c3dd4cbbd663fc9942ba8e379c900b5183b687
TSdnl13 <daniel.int@outlook.com>
2022-07-28 17:10:58 -0500

    Add: ProductRoutes.js file that handle post request methods for crud actions

---
+++ backend/src/routes/ProductRoutes.js
@@ -0,0 +1,14 @@
+import { Router } from 'express';
+import { addNewProduct, deleteProduct, getAllProducts, getProductById, getProductsByCategory, modifyProduct } from '../controllers/ProductController.js';
+import auth from '../middlewares/auth.js';
+
+const router = Router();
+
+router.get('/', getAllProducts);
+router.get('/category/:categoryId', getProductsByCategory);
+router.get('/:productId', getProductById);
+router.post('/', auth, addNewProduct);
+router.put('/:productId', auth, modifyProduct);
+router.delete('/:productId', auth, deleteProduct);
+
+export default router;
