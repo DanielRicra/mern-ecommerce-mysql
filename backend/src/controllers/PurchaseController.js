5c33f79de67a7efc196d2dec413a0a8587f48b6f
TSdnl13 <daniel.int@outlook.com>
2022-07-31 16:12:06 -0500

    Added PurchaseController.js file
    This file has the controllers for the purchases routes, the functions in
    this file manipulate the request and return with a appropiate response.

---
+++ backend/src/controllers/PurchaseController.js
@@ -0,0 +1,58 @@
+import { format } from 'date-fns';
+import { saveAllPurchaseProducts } from '../services/PurchaseProductsService.js';
+import { countPurchases, findAllPurchases, savePurchase } from '../services/PurchaseService.js';
+
+const paymentTypes = ['cash', 'paypal', 'credit_card', 'debit_card'];
+
+export const getAllPurchases = async (req, res) => {
+   const { page =  1 } = req.query;
+   try {
+      const [countResult] = await countPurchases();
+      const totaPages = Math.ceil(countResult[0].purchases_count/20);
+
+      if (isNaN(page) || (!isNaN(page) && page < 1 || page > totaPages)) {
+         return res.status(404).json({ message: 'Page not found' });
+      }
+
+      const [resultPurchases] = await findAllPurchases(page);
+      
+      res.status(200).json({ 
+         page: Number(page),
+         results: resultPurchases[0].results,
+         total_results: countResult[0].purchases_count,
+         total_pages: totaPages
+      });
+   } catch (error) {
+      return res.status(500).json({ message: error.message });
+   }
+};
+
+export const addPurchase = async (req, res) => {
+   const currentDateTime = format(new Date(), 'yyyy-MM-dd\'T\'HH:mm:ss');
+   const { userId, purchaseDate = currentDateTime, paymentType, comment, state, items = [] } = req.body;
+
+   if (!paymentType || !paymentTypes.includes(paymentType.toLowerCase()) ||
+      items.length === 0) {
+      return res.status(400).json({ message: 'Invalid payment type or empty items list' });
+   }
+
+   let productsValues = '';
+   
+   try {
+      const [saveResult] = await savePurchase({userId, purchaseDate, paymentType, comment, state});
+      
+      for (let i = 0; i < items.length; i++) {
+         if (!items[i].productId || !items[i].quantity || !items[i].total) {
+            return res.status(400).json({ message: 'Invalid product data' });
+         }
+         productsValues += `(${saveResult.insertId}, ${items[i].productId}, ${items[i].quantity}, ${items[i].total})${i === (items.length - 1) ? '': ','}`;
+      }
+
+      await saveAllPurchaseProducts(productsValues);
+      
+      return res.status(201).json({purchase_id: saveResult.insertId, userId, purchaseDate, paymentType, comment, state});
+   } catch (error) {
+      return res.status(500).json({ message: error.message });
+   }
+   
+};
