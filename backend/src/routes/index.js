3467c996d8ec815bdcbbffb6a358c87e85a83643
TSdnl13 <daniel.int@outlook.com>
2022-07-31 16:22:09 -0500

    added index.js file in routes folder, this file is responsible for exporting the other rutes in this folder

---
+++ backend/src/routes/index.js
@@ -0,0 +1,4 @@
+export { default as userRouter } from './UserRoutes.js';
+export { default as productRouter } from './ProductRoutes.js';
+export { default as purchaseRouter } from './PurchaseRoutes.js';
+export { default as categoryRouter } from './CategoryRoutes.js';
