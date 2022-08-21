46e5002ae9823af031074ee388d0a61b9f05c3d0
TSdnl13 <daniel.int@outlook.com>
2022-08-21 09:50:35 -0500

    add routes, navbar and colorschemeprovider
    rotes: '/','/auth', '*'.
    
    ColorSchemeProvider from mantine that help to switch the app color theme
    using useAppTheme and with keyboard with useHotKetys

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,38 @@
 import React from 'react';
+import { Route, Routes } from 'react-router-dom';
+import {
+  ColorSchemeProvider, MantineProvider, Paper,
+} from '@mantine/core';
+import { useHotkeys } from '@mantine/hooks';
 import './App.css';
+import useAppTheme from './hooks/useAppTheme';
+import HeaderSearch from './layouts/header/Header';
+import Home from './pages/home/Home';
+import NothingFoundBackground from './pages/404/NothingFoundBackground';
+import Authentication from './pages/auth/Authentication';
 
 function App() {
+  const { colorScheme, toggleColorScheme } = useAppTheme();
+
+  useHotkeys([['mod+J', () => toggleColorScheme()]]);
+
   return (
-    <div className="App">
-      App
-    </div>
+    <ColorSchemeProvider colorScheme={colorScheme} toggleColorScheme={toggleColorScheme}>
+      <MantineProvider
+        theme={{ colorScheme, fontFamily: 'Mukta, sans-serif' }}
+        withGlobalStyles
+        withNormalizeCSS
+      >
+        <Paper radius={0} style={{ minHeight: '100vh' }}>
+          <HeaderSearch />
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/auth" element={<Authentication />} />
+            <Route path="*" element={<NothingFoundBackground />} />
+          </Routes>
+        </Paper>
+      </MantineProvider>
+    </ColorSchemeProvider>
   );
 }
 
