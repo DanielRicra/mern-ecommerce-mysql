9a87e7591165f5bb52c414c4b9b131b57893c128
TSdnl13 <daniel.int@outlook.com>
2022-08-22 16:01:01 -0500

    add simple product card with image, name and price

---
+++ frontend/src/components/products/product/Product.tsx
@@ -0,0 +1,44 @@
+import {
+  Card,
+  CardSection,
+  Group,
+  Image,
+  Text,
+} from '@mantine/core';
+import React from 'react';
+import { ProductProps } from '../../../types/types';
+import formatMoney from '../../../utils/format-money';
+
+function Product({ product }: ProductProps) {
+  return (
+    <Card
+      shadow="sm"
+      p="md"
+      radius="md"
+      withBorder
+      key={product.product_id}
+      style={{ minWidth: '240px', cursor: 'pointer' }}
+    >
+      <CardSection>
+        <Image
+          fit="cover"
+          src={product.img_url ? product.img_url : undefined}
+          height={160}
+          width={240}
+          alt={product.name}
+          withPlaceholder
+        />
+      </CardSection>
+
+      <Group position="apart" mt="md" mb="xs">
+        <Text weight={500}>{product.name}</Text>
+      </Group>
+
+      <Text size="lg">
+        {formatMoney(Number(product.sale_price))}
+      </Text>
+    </Card>
+  );
+}
+
+export default Product;
