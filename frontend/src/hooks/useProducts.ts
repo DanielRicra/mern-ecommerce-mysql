31e9202bd2410742e86ffeaacfe08dbd2a36a151
TSdnl13 <daniel.int@outlook.com>
2022-08-22 22:21:18 -0500

    add productReducer in custom hook
    productreducer will handle error, loading and productResponse when
    fetching the products data.
    
    the useProducts function now return loading, error, setPage and
    productResponse.
    
    add try catch when fetching with findAllProduct service.

--- frontend/src/hooks/useProducts.ts
+++ frontend/src/hooks/useProducts.ts
@@ -1,27 +1,23 @@
-import { useState, useEffect } from 'react';
-import { findAllProducts } from '../../services/services';
-import { ProductsResponse } from '../../types/types';
-
-const defaultProductResponse: ProductsResponse = {
-  page: 0,
-  results: [],
-  total_pages: 0,
-  total_results: 0,
-};
+import { useState, useEffect, useReducer } from 'react';
+import productsReducer, { INITIAL_STATE } from '../reducers/productsReducer';
+import { findAllProducts } from '../services/services';
+import { ProductsActionsKind } from '../types/types';
 
 const useProducts = () => {
-  const [productsResponse, setProductsResponse] = useState<ProductsResponse>(
-    defaultProductResponse,
-  );
-
+  const [state, dispatch] = useReducer(productsReducer, INITIAL_STATE);
   const [page, setPage] = useState(1);
 
   useEffect(() => {
     let isCancelled = false;
     async function getAllProducts(p: number) {
-      const response = await findAllProducts(p);
-      if (!isCancelled) {
-        setProductsResponse(response.data);
+      dispatch({ type: ProductsActionsKind.FETCH_START });
+      try {
+        const response = await findAllProducts(p);
+        if (!isCancelled) {
+          dispatch({ type: ProductsActionsKind.FETCH_SUCCESS, payload: response.data });
+        }
+      } catch (error) {
+        dispatch({ type: ProductsActionsKind.FETCH_ERROR });
       }
     }
     getAllProducts(page);
@@ -31,7 +27,12 @@ const useProducts = () => {
     };
   }, [page]);
 
-  return { productsResponse, setPage };
+  return {
+    loading: state.loading,
+    error: state.error,
+    productsResponse: state.productResponse,
+    setPage,
+  };
 };
 
 export default useProducts;
