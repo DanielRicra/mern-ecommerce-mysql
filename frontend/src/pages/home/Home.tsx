38f3d6b139f1611b5432fc81b04e7d53b3b82942
TSdnl13 <daniel.int@outlook.com>
2022-08-22 22:25:07 -0500

    add loader and error code when fetching data
    the loader will show when the fetch has stated.
    
    the product list will show when the fetch has retorned a succes
    response.
    
    the error will show when there is an error while fetching.

--- frontend/src/pages/home/Home.tsx
+++ frontend/src/pages/home/Home.tsx
@@ -1,31 +1,58 @@
 import React from 'react';
 import {
+  Alert,
   Center,
   Container,
+  Loader,
   Pagination,
   Paper,
   Title,
 } from '@mantine/core';
-import useProducts from './useProducts';
+import { IconAlertCircle } from '@tabler/icons';
+
+import useProducts from '../../hooks/useProducts';
 import ProductList from '../../components/products/ProductList';
 
 function Home() {
-  const { productsResponse, setPage } = useProducts();
+  const {
+    loading,
+    error,
+    productsResponse,
+    setPage,
+  } = useProducts();
 
   return (
     <Container size={1080} py={36}>
       <Title order={2} style={{ fontSize: '22px' }} pl={4} mb={18}>Results</Title>
       <Paper mb={18}>
-        <ProductList products={productsResponse.results} />
-
-        <Center my={32}>
-          <Pagination
-            total={productsResponse.total_pages}
-            color="violet"
-            radius="md"
-            onChange={(page: number) => setPage(page)}
-          />
-        </Center>
+        {loading && <Center><Loader size="xl" color="violet" variant="bars" /></Center>}
+        {(!error && !loading) && (
+          <>
+            <ProductList products={productsResponse.results} />
+            <Center my={32}>
+              <Pagination
+                total={productsResponse.total_pages}
+                color="violet"
+                radius="md"
+                page={productsResponse.page}
+                onChange={(page: number) => setPage(page)}
+              />
+            </Center>
+          </>
+        )}
+        {error && (
+          <Center>
+            <Alert
+              icon={<IconAlertCircle size={18} />}
+              title="Error"
+              color="red"
+              variant="outline"
+              radius="md"
+            >
+              Something bad happend, refresh the page or try again later
+            </Alert>
+          </Center>
+        )}
       </Paper>
     </Container>
   );
