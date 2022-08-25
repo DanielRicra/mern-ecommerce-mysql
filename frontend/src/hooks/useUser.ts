53afc68bdfbd29e4eb0f4989f034c24b3838fb9d
TSdnl13 <daniel.int@outlook.com>
2022-08-24 23:14:37 -0500

    renamed file from useUser to hooks

--- frontend/src/hooks/useUser.ts
+++
@@ -1,5 +0,0 @@
-import { TypedUseSelectorHook, useDispatch, useSelector } from 'react-redux';
-import { RootState, AppDispatch } from '../app/store';
-
-export const useUserDispatch = () => useDispatch<AppDispatch>();
-export const useUserSelector: TypedUseSelectorHook<RootState> = useSelector;
