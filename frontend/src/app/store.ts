ff345faacc66add5321f17f95e8f28573aa67b83
TSdnl13 <daniel.int@outlook.com>
2022-08-25 21:23:24 -0500

    add cartReducer to store with name cart

--- frontend/src/app/store.ts
+++ frontend/src/app/store.ts
@@ -1,9 +1,11 @@
 import { configureStore } from '@reduxjs/toolkit';
 import userReducer from '../features/user/user-slice';
+import cartReducer from '../features/cart/cart-slice';
 
 export const store = configureStore({
   reducer: {
     user: userReducer,
+    cart: cartReducer,
   },
 });
 
