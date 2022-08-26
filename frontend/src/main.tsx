f9bf095bf3e8bc2677691ccb72530c5bdaa19a62
TSdnl13 <daniel.int@outlook.com>
2022-08-25 21:13:04 -0500

    add subscribe method to store to save cart state in local storage

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -5,6 +5,12 @@ import { Provider } from 'react-redux';
 import { store } from './app/store';
 import App from './App';
 import './index.css';
+import { saveCartState } from './utils/local-storage';
+import { CartProduct } from './types/types';
+
+store.subscribe(() => {
+  saveCartState<CartProduct[]>('shopping-cart', store.getState().cart.cartProducts);
+});
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
