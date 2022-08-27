39e23fabb390f88ff359781ecc621da14757ed1f
TSdnl13 <daniel.int@outlook.com>
2022-08-26 22:33:06 -0500

    add cart reducer, with addToCart and updateQuantity.. reducers

---
+++ frontend/src/features/cart/cart-slice.ts
@@ -0,0 +1,52 @@
+/* eslint-disable no-param-reassign */
+import { createSlice, PayloadAction } from '@reduxjs/toolkit';
+import { CartProduct, CartProducts } from '../../types/types';
+import { getCartProducts } from '../../utils/local-storage';
+
+interface UpdateQuantityPayload {
+  productId: number;
+  quantity: number;
+}
+
+const initialState: CartProducts = {
+  cartProducts: getCartProducts(),
+};
+
+const cartSlice = createSlice({
+  name: 'cart',
+  initialState,
+  reducers: {
+    addToCart: (state, action: PayloadAction<CartProduct>) => {
+      const { payload } = action;
+
+      if (state.cartProducts.find((cp) => cp.product.product_id === payload.product.product_id)
+        === undefined) {
+        state.cartProducts = [...state.cartProducts, payload];
+      } else {
+        state.cartProducts = state.cartProducts.map((cp) => {
+          if (cp.product.product_id === payload.product.product_id) {
+            return { ...cp, quantity: cp.quantity + payload.quantity };
+          }
+          return cp;
+        });
+      }
+    },
+    updateQuantityFromCart: (state, action: PayloadAction<UpdateQuantityPayload>) => {
+      const { productId, quantity } = action.payload;
+
+      if (quantity === 0) {
+        state.cartProducts = state.cartProducts.filter((cp) => cp.product.product_id !== productId);
+      } else {
+        state.cartProducts = state.cartProducts.map((cp) => {
+          if (cp.product.product_id === productId) {
+            return { ...cp, quantity };
+          }
+          return cp;
+        });
+      }
+    },
+  },
+});
+
+export const { addToCart, updateQuantityFromCart } = cartSlice.actions;
+export default cartSlice.reducer;
