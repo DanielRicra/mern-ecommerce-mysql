b21f3088a874a917ceb46e1921289b3c85d38d64
TSdnl13 <daniel.int@outlook.com>
2022-08-30 16:58:38 -0500

    add searchRother import to export in this index file

--- backend/src/routes/index.js
+++ backend/src/routes/index.js
@@ -2,3 +2,4 @@ export { default as userRouter } from './UserRoutes.js';
 export { default as productRouter } from './ProductRoutes.js';
 export { default as purchaseRouter } from './PurchaseRoutes.js';
 export { default as categoryRouter } from './CategoryRoutes.js';
+export { default as searchRouter } from './SearchRoutes.js';
