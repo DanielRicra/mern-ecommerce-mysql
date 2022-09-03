9c5130ff03e90debf1d8696214eea4d062698e3d
TSdnl13 <daniel.int@outlook.com>
2022-09-02 23:06:24 -0500

    add products reducer from profuctsSlice

--- frontend/src/app/store.ts
+++ frontend/src/app/store.ts
@@ -1,11 +1,13 @@
 import { configureStore } from '@reduxjs/toolkit';
 import userReducer from '../features/user/user-slice';
 import cartReducer from '../features/cart/cart-slice';
+import productsSlice from '../features/products/products-slice';
 
 export const store = configureStore({
   reducer: {
     user: userReducer,
     cart: cartReducer,
+    products: productsSlice,
   },
 });
 
