21bc876c6d0c444091c3d6c89b8e6d84a8350556
TSdnl13 <daniel.int@outlook.com>
2022-09-07 20:54:41 -0500

    add count and get  purchases by userId functions

--- backend/src/services/PurchaseService.js
+++ backend/src/services/PurchaseService.js
@@ -26,6 +26,35 @@ export const countPurchases = async () => {
    return await pool.query('SELECT COUNT(purchase_id) as purchases_count FROM purchases');
 };
 
+export const findPurchasesByUserId = async (userId, page = 1) => {
+   return await pool.query(
+      `SELECT 
+         JSON_ARRAYAGG(JSON_OBJECT(
+            'purchaseId', P.purchase_id, 'user_id', P.user_id, 'purchaseDate', P.purchase_date, 'comment', P.comment, 'status', P.status, 'paymentType', P.payment_type , 'products', PP.products
+         )) as results
+      FROM purchases P
+         INNER JOIN (
+            SELECT
+               pp.purchase_id,
+               JSON_ARRAYAGG(JSON_OBJECT(
+                  'productId', pp.product_id, 'totalPrice', pp.total, 'quantity', pp.quantity ,'name', Pr.name, 'description', Pr.description, 'unitSalePrice', Pr.sale_price, 'barCode', Pr.bar_code, 'categoryId', Pr.category_id
+               )) products
+            FROM purchase_products pp
+               INNER JOIN products Pr ON Pr.product_id=pp.product_id
+            GROUP BY pp.purchase_id
+         ) PP ON PP.purchase_id=P.purchase_id
+      WHERE P.user_id= ? LIMIT ${(page - 1) * 20}, 20`,
+      [userId]
+   );
+};
+
+export const countPurchasesByUserId = async (userId) => { 
+   return await pool.query(
+      'SELECT COUNT(purchase_id) as purchases_count FROM purchases WHERE user_id = ?',
+      [userId]
+   );
+};
+
 export const savePurchase = async ({purchaseDate, comment, status, userId, paymentType}) => {
    return await pool.query(
       'INSERT INTO purchases (purchase_date, comment, status, user_id, payment_type) ' + 
