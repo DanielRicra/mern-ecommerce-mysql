f6da508416a29daa5a3b8246e7f1ea5c382c3b58
TSdnl13 <daniel.int@outlook.com>
2022-09-07 20:52:58 -0500

    add /:userId path with controller getPurchasesByUserID

--- backend/src/routes/PurchaseRoutes.js
+++ backend/src/routes/PurchaseRoutes.js
@@ -1,10 +1,15 @@
 import { Router } from 'express';
-import { getAllPurchases, addPurchase } from '../controllers/PurchaseController.js';
+import {
+   getAllPurchases,
+   addPurchase,
+   getPurchasesByUserId
+} from '../controllers/PurchaseController.js';
 import auth from '../middlewares/auth.js';
 
 const router = Router();
 
 router.get('/', getAllPurchases);
+router.get('/:userId', getPurchasesByUserId);
 router.post('/', auth, addPurchase);
 
 export default router;
