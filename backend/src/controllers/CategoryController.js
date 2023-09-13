faaeb87bb167966389929165ef9161d6c0db18a0
DanielRicra <daniel.int@outlook.com>
2023-09-13 15:44:51 -0500

    Refactor CategoryController to make it a class
    - Receive a categoryModel as an argument in constructor.

--- backend/src/controllers/CategoryController.js
+++ backend/src/controllers/CategoryController.js
@@ -1,31 +1,35 @@
-import { findAllCategories, findCategoryByName, saveCategory } from '../services/CategoryService.js';
-
-
-export const getAllCategories = async (req, res) => {
-   try {
-      const [result] = await findAllCategories();
-      return res.status(200).json({ results: result });
-   } catch (error) {
-      return res.status(500).json({ message: error.message });
-   }
-};
-
-export const addCategory = async (req, res) => {
-   const { name } = req.body;
-   
-   if (!name) {
-      return res.status(400).json({ message: 'Name must be provided' });
+export class CategoryController {
+   constructor(categoryModel) {
+      this.categoryModel = categoryModel;
    }
 
-   try {
-      const [foundedResult] = await findCategoryByName(name);
-      if (foundedResult.length > 0) {
-         return res.status(409).json({ message: `category with name '${name}' already exists` });
+   getAll = async (req, res) => {
+      try {
+         const categories = await this.categoryModel.getAll();
+         res.status(200).json({ results: categories });
+      } catch (error) {
+         return res.status(500).json({ message: error.message });
       }
+   };
 
-      const [result] = await saveCategory(name);
-      return res.status(201).json({ name, categoryId: result.insertId });
-   } catch (error) {
-      return res.status(500).json({ message: error.message });
-   }
-};
+   create = async (req, res) => {
+      const { name } = req.body;
+
+      if (!name) {
+         return res.status(400).json({ message: 'Name must be provided' });
+      }
+      try {
+         const existingCategory = await this.categoryModel.getByName({ name });
+         if (existingCategory) {
+            return res.status(409).json({
+               message: `category with name '${name}' already exists`,
+            });
+         }
+
+         const newCategory = await this.categoryModel.create({ name });
+         res.status(201).json({ name, categoryId: newCategory.insertId });
+      } catch (error) {
+         res.status(500).json({ message: error.message });
+      }
+   };
+}
