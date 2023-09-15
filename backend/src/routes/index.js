fd3408e794c3e56f493ba7e6d96c0e34481809d6
DanielRicra <daniel.int@outlook.com>
2023-09-14 22:34:37 -0500

    Update imported file names

--- backend/src/routes/index.js
+++ backend/src/routes/index.js
@@ -1,5 +1,4 @@
 export { default as userRouter } from './UserRoutes.js';
-export { default as productRouter } from './ProductRoutes.js';
+export * from './product.js';
 export { default as purchaseRouter } from './PurchaseRoutes.js';
-export { default as categoryRouter } from './CategoryRoutes.js';
-export { default as searchRouter } from './SearchRoutes.js';
+export * from './category.js';
