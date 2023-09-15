8ea14f4f457fb3af993e7ca2b811aaf3d66bed91
DanielRicra <daniel.int@outlook.com>
2023-09-14 22:19:41 -0500

    Refactor ProductController to use classes
    - Now all the methods are part of class ProductController

--- backend/src/controllers/ProductController.js
+++ backend/src/controllers/ProductController.js
@@ -1,150 +1,150 @@
-import { 
-   deleteProductById,
-   findAllProducts,
-   countProducts, 
-   findProductById, 
-   saveProduct, 
-   updateProduct, 
-   findProductsByCategory,
-   countProductsByCategory
-} from '../services/ProductService.js';
-import { isValidProduct } from '../validations/ProductValidations.js';
+import {
+   validatePartialProduct,
+   validateProduct,
+   validateQuery,
+} from '../schemas/product.js';
 
-export const getAllProducts = async (req, res) => {
-   const { page = 1} = req.query;
- 
-   try {
-      const [resultCount] = await countProducts();
-      const totalPages = Math.ceil(resultCount[0].count_products/20);
+export class ProductController {
+   constructor(productModel) {
+      this.productModel = productModel;
+   }
 
-      if (isNaN(page) || (!isNaN(page) && page < 1 || page > totalPages)) {
-         return res.status(404).json({ message: 'Page not found' });
+   getAll = async (req, res) => {
+      const result = validateQuery(req.query);
+
+      if (!result.success) {
+         return res
+            .status(400)
+            .json({ message: JSON.parse(result.error.message) });
+      }
+      const { name, page, categoryId, take } = result.data;
+
+      try {
+         const count = await this.productModel.count({ categoryId, name });
+         const totalPages = Math.ceil(count / take);
+
+         if (page < 1 || page > totalPages) {
+            return res.status(404).json({ message: 'Page not found' });
+         }
+
+         const products = await this.productModel.getAll({
+            page,
+            name,
+            categoryId,
+            take,
+         });
+
+         res.status(200).json({
+            page: Number(page),
+            results: products,
+            total_results: count,
+            total_pages: totalPages,
+         });
+      } catch (error) {
+         return res.status(500).json({ message: 'Something went wrong' });
+      }
+   };
+
+   getById = async (req, res) => {
+      const { productId } = req.params;
+
+      try {
+         const product = await this.productModel.getById({ productId });
+
+         if (!product) {
+            return res.status(404).json({ message: 'Product not found' });
+         }
+
+         return res.status(200).json(product);
+      } catch (error) {
+         return res.status(500).json({ message: 'Something went wrong' });
+      }
+   };
+
+   // TODO: find category first, to enhance handle error
+   create = async (req, res) => {
+      const result = validateProduct(req.body);
+
+      if (!result.success) {
+         return res
+            .status(400)
+            .json({ message: JSON.parse(result.error.message) });
       }
 
-      const [productsResult] = await findAllProducts(page);
+      try {
+         const newProduct = await this.productModel.create({
+            product: result.data,
+         });
 
-      res.status(200).json({ 
-         page: Number(page),
-         results: productsResult,
-         total_results: resultCount[0].count_products,
-         total_pages: totalPages
-      });
-   } catch (error) {
-      return res.status(500).json({ message: 'Something went wrong: ' + error.message });
-   }
-};
+         return res.status(201).json(newProduct);
+      } catch (error) {
+         return res.status(500).json({ message: 'Something went wrong' });
+      }
+   };
 
-export const getProductById = async (req, res) => {
-   const { productId } = req.params;
+   // TODO: find category first, to enhance handle error
+   update = async (req, res) => {
+      const { productId } = req.params;
 
-   try {
-      const [result] = await findProductById(productId);
+      const result = validatePartialProduct(req.body);
 
-      if (result.length === 0) {
-         return res.status(404).json({ message: 'Product not found' });
+      if (!result.success) {
+         return res
+            .status(400)
+            .json({ message: JSON.parse(result.error.message) });
       }
 
-      return res.status(200).json({ ...result[0] });
-   } catch (error) {
-      return res.status(500).json({ message: error.message });
-   }
-};
+      try {
+         const existingProduct = await this.productModel.getById({ productId });
 
-export const getProductsByCategory = async (req, res) => {
-   const { categoryId } = req.params;
-   const { page = 1} = req.query;
+         if (!existingProduct) {
+            return res.status(404).json({ message: 'Product not found' });
+         }
 
-   try {
-      const [resultCount] = await countProductsByCategory(categoryId);
-      const totalPages = Math.ceil(resultCount[0].count_products/20);
+         const {
+            description,
+            barCode,
+            categoryId,
+            imgUrl,
+            name,
+            salePrice,
+            stock,
+         } = result.data;
 
-      if (isNaN(page) || (!isNaN(page) && page < 1 || page > totalPages) || isNaN(categoryId)) {
-         return res.status(404).json({ message: 'Page not found' });
+         const mappedProduct = {};
+         if (name) mappedProduct.name = name;
+         if (description) mappedProduct.description = description;
+         if (categoryId) mappedProduct.category_id = categoryId;
+         if (barCode) mappedProduct.bar_code = barCode;
+         if (salePrice) mappedProduct.sale_price = salePrice;
+         if (stock) mappedProduct.stock = stock;
+         if (imgUrl) mappedProduct.img_url = imgUrl;
+
+         const updatedProduct = await this.productModel.update({
+            productId,
+            product: { ...mappedProduct },
+         });
+
+         return res.status(200).json(updatedProduct);
+      } catch (error) {
+         console.log(error);
+         return res.status(500).json({ message: 'Something went wrong' });
       }
+   };
 
-      const [result] = await findProductsByCategory(categoryId, page);
+   delete = async (req, res) => {
+      const { productId } = req.params;
 
-      if (result.length === 0) {
-         return res.status(404).json({ message: 'There is no products with that category id' });
+      try {
+         const wasDeleted = await this.productModel.delete({ productId });
+
+         if (!wasDeleted) {
+            return res.status(404).json({ message: 'Product not found' });
+         }
+
+         return res.sendStatus(204);
+      } catch (error) {
+         return res.status(500).json({ message: 'Something went wrong' });
       }
-
-      return res.status(200).json({ 
-         page: Number(page), 
-         results: result,  
-         total_results: resultCount[0].count_products,
-         total_pages: totalPages
-      });
-
-   } catch (error) {
-      return res.status(500).json({ message: error.message });
-   }
-};
-
-export const addNewProduct = async (req, res) => {
-   const product = req.body;
-
-   if (!isValidProduct(product)) {
-      return res.status(400).json({ message: 'Invalid Data' });
-   }
-
-   try {
-      const [result] = await saveProduct(product);
-
-      return res.status(201).json({...product, productId: result.insertId });
-   } catch (error) {
-      return res.status(500).json({ message: 'Something went wrong: ' + error.message });
-   }
-};
-
-export const modifyProduct = async (req, res) => {
-   const { productId } = req.params; 
-   const { name, description, categoryId, barCode, salePrice, stock } = req.body;
-
-   if (!isValidProduct({name, categoryId, salePrice, stock})) {
-      return res.status(400).json({ message: 'Invalid Data' });
-   }
-
-   try {
-      const [resultFind] = await findProductById(productId);
-
-      if (resultFind.length === 0) {
-         return res.status(404).json({ message: 'Product to update cannot be found'});
-      }
-
-      const modifiedProduct = {
-         name, 
-         description, 
-         category_id: categoryId, 
-         bar_code: barCode, 
-         sale_price: salePrice, 
-         stock 
-      };
-
-      await updateProduct(modifiedProduct, productId);
-
-      return res.status(200).json({...resultFind[0], ...modifiedProduct});
-
-   } catch (error) {
-      return res.status(500).json({ message: 'Something went wrong: ' + error.message });
-   }
-};
-
-export const deleteProduct = async (req, res) => {
-   const { productId } = req.params;
-
-   if (isNaN(productId)) {
-      return res.sendStatus(400);
-   }
-
-   try {
-      const [result] = await deleteProductById(productId); 
-      
-      if (result.affectedRows === 0){
-         return res.status(404).json({ message: 'Product not found' });
-      }
-
-      return res.sendStatus(204);
-   } catch (error) {
-      return res.status(500).json({ message: 'Something went wrong: ' + error.message });
-   }
-};
+   };
+}
