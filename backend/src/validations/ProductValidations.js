f9915efe4d33f6e971959ede0686df344c3143e9
DanielRicra <daniel.int@outlook.com>
2023-09-14 22:35:28 -0500

    Delete unused ProductValidation.js
    -A better logic is now handled in product schema with zod.

--- backend/src/validations/ProductValidations.js
+++
@@ -1,9 +0,0 @@
-
-export const isValidProduct = ({ name, categoryId, salePrice, stock }) => {
-   if (!name || !categoryId || !salePrice || !stock) {
-      return false;
-   }
-   
-   if (name.trim() === '' ) return false;
-   return true;
-};
