cd19ba9d1992ed47745e52fa9320a7bcd2f25cfc
DanielRicra <daniel.int@outlook.com>
2023-09-18 11:59:59 -0500

    Update product routers test.
    - Because the controllers were refactored.

--- backend/tests/ProductRouters.test.js
+++ backend/tests/ProductRouters.test.js
@@ -9,68 +9,69 @@ describe('GET /products', () => {
       await api.get('/api/products').expect(200).expect('Content-type', /json/);
    });
 
-   test('should respond with an JSON object', async () => { 
+   test('should respond with an JSON object', async () => {
       const response = await api.get('/api/products');
       expect(response.body).toBeInstanceOf(Object);
    });
 
-   test('should have results property in response', async () => { 
+   test('should have results property in response', async () => {
       const response = await api.get('/api/products').send();
+      console.log(response.body);
       expect(response.body.results).toBeDefined();
    });
 
-   describe('GET /products/:producId', () => {
-      test('should responde with a 200 status code', async () => { 
-         await api.get('/api/products/1')
+   describe('GET /products/:productId', () => {
+      test('should respond with a 200 status code', async () => {
+         await api
+            .get('/api/products/1')
             .expect(200)
             .expect('Content-type', /json/);
       });
 
-      test('should respond with  JSON object', async () => { 
+      test('should respond with  JSON object', async () => {
          const response = await api.get('/api/products/1').send();
          expect(response.body).toBeInstanceOf(Object);
       });
 
-      test('should respond with  404 status code', async () => { 
+      test('should respond with  404 status code', async () => {
          await api.get('/api/products/0').expect(404);
       });
    });
 
-   describe('GET /products/category/:categoryId', () => {
-      test('should responde with a 200 status code', async () => { 
-         await api.get('/api/products/category/1')
+   describe('GET /products?categoryId', () => {
+      test('should respond with a 200 status code', async () => {
+         await api
+            .get('/api/products?categoryId=1')
             .expect(200)
             .expect('Content-type', /json/);
       });
 
-      test('should respond with  JSON object', async () => { 
-         const response = await api.get('/api/products/category/1').send();
+      test('should respond with  JSON object', async () => {
+         const response = await api.get('/api/products?categoryId=1').send();
          expect(response.body).toBeInstanceOf(Object);
       });
 
-      test('should respond with  404 status code', async () => { 
-         await api.get('/api/products/category/0').expect(404);
-         await api.get('/api/products/category/hf').expect(404);
+      test('should respond with  404 or 400 status code', async () => {
+         await api.get('/api/products?categoryId=150').expect(404);
+         await api.get('/api/products?categoryId=h1f').expect(400);
       });
    });
 
-   describe('When page query parameter is not a numbert or wrong page',  () => {
-      test('should respond with status code 404', async () => { 
-         await api.get('/api/products?page=text').expect(404);
+   describe('When page query parameter', () => {
+      test('should respond with status code 400', async () => {
+         await api.get('/api/products?page=text').expect(400);
          await api.get('/api/products?page=0').expect(404);
       });
    });
 });
 
 describe('POST /products', () => {
-   
    describe('When there is no authorization token', () => {
-      test('should respond with a 401 status code', async () => { 
+      test('should respond with a 401 status code', async () => {
          const response = await api.post('/api/products').send();
          expect(response.statusCode).toBe(401);
       });
    });
-   
 });
 
 afterAll(async () => {
