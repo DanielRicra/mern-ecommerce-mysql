80c3f0e0efca4659f09059ca45be2dcc67894d76
DanielRicra <daniel.int@outlook.com>
2023-09-17 22:41:29 -0500

    Delete unused services, replace by models

--- backend/src/services/PurchaseProductsService.js
+++
@@ -1,7 +0,0 @@
-import { pool } from '../config/database.js';
-
-export const saveAllPurchaseProducts = async (queryData) => {
-   return await pool.query(
-      `INSERT INTO purchase_products (purchase_id, product_id, quantity, total) VALUES ${queryData}`
-   );
-};
