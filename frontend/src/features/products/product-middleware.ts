c812ad5b95a1ddae7b46f63834f1db09c41fa538
DanielRicra <daniel.int@outlook.com>
2023-09-18 21:00:51 -0500

    Refactor to use findAllProducts
    - improve error handling

--- frontend/src/features/products/product-middleware.ts
+++ frontend/src/features/products/product-middleware.ts
@@ -1,28 +1,34 @@
 import { createAsyncThunk } from '@reduxjs/toolkit';
 import { AxiosError } from 'axios';
-import { findProductsByNameAndCategory } from '../../services/services';
 import { ProductsResponse } from '../../types/types';
+import { findAllProducts } from '../../services/services';
 
 type FetchError = {
-  errorMessage: string;
+   errorMessage: string;
 };
 
 const getProductsByNameAndCategory = createAsyncThunk<
-ProductsResponse,
-{ name: string; categoryId: number; page: number },
-{ rejectValue: FetchError; }
+   ProductsResponse,
+   { name: string; categoryId: number; page: number },
+   { rejectValue: FetchError }
 >('products/getProductByName', async ({ name, categoryId, page }, thunkApi) => {
-  try {
-    const response = await findProductsByNameAndCategory(name, categoryId, page);
-    return response.data;
-  } catch (err) {
-    // eslint-disable-next-line @typescript-eslint/no-explicit-any
-    const error: AxiosError<FetchError> = err as any;
-    if (!error.response) {
-      throw err;
-    }
-    return thunkApi.rejectWithValue(error.response.data);
-  }
+   try {
+      const response = await findAllProducts({ name, categoryId, page });
+      return response.data;
+   } catch (err) {
+      if (err instanceof AxiosError) {
+         if (err.response?.status === 404) {
+            return thunkApi.rejectWithValue({
+               errorMessage: 'Product not found',
+            });
+         }
+         return thunkApi.rejectWithValue({
+            errorMessage: err.response?.data.message,
+         });
+      }
+
+      return thunkApi.rejectWithValue({ errorMessage: 'Something went wrong' });
+   }
 });
 
 export default getProductsByNameAndCategory;
